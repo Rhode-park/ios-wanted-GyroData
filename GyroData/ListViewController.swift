ccd78e80483ff26bd8279354c5cb63d66a7489e1
Rhode-park <jinah.park.16@gmail.com>
2023-06-12 15:00:22 +0900

    feat: configureUI 메서드 구현을 통한 title 구현, 측정 버튼 구현

--- GyroData/ListViewController.swift
+++ GyroData/ListViewController.swift
@@ -11,9 +11,20 @@ final class ListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        configureUI()
     }
     
+    private func configureUI() {
+        view.backgroundColor = .white
+        title = "목록"
+        
+        let measureButton = UIBarButtonItem(title: "측정", style: .plain, target: self, action: #selector(measureData))
+        navigationItem.rightBarButtonItem = measureButton
+    }
     
+    @objc
+    private func measureData() {
+        
+    }
 }
 
